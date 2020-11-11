d1486b3504e3981e8b7b73d6fd55b0a12a38005d
eryk <eryk.malczyk@bitcomp.fi>
2020-11-11 15:40:54 +0100

    Refactor functional_tests to use class based tests

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,24 @@
+import unittest
 from selenium import webdriver
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
 
-assert 'Django' in browser.title
+class NewVisitorTest(unittest.TestCase):
+    """
+    Basic test class
+    """
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_start_list_and_retrieve_it_later(self):
+        # User checks if webapp opens
+        self.browser.get('http://localhost:8000')
+
+        # User checks if title has to-do in it
+        self.assertIn('To-Do', self.browser.title)
+        self.fail('Finish test!')
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
