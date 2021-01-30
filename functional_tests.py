5ae5fcb5869c5dfe194f14b1e79ec597fd26e278
eryk <eryk.malczyk@bitcomp.fi>
2021-01-30 23:29:43 +0100

    Basic view returns minimal html

--- functional_tests.py
+++ functional_tests.py
@@ -20,5 +20,6 @@ class NewVisitorTest(unittest.TestCase):
         self.assertIn('To-Do', self.browser.title)
         self.fail('Finish test!')
 
+
 if __name__ == '__main__':
     unittest.main(warnings='ignore')
