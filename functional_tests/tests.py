12438daa073f1588881ba1f86f84597c3e5a4a59
eryk <eryk.malczyk@bitcomp.fi>
2021-03-27 17:16:43 +0100

    Added checking of inputbox alignment in home and list template

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -68,6 +68,30 @@ class NewVisitorTest(LiveServerTestCase):
         self.assertNotIn('Buy peacock feathers', page_text)
         self.assertIn('Buy milk', page_text)
 
+    def test_layout_and_styling(self):
+        # we visit home page
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024, 768)
+
+        # Check if input box is centered
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512,
+            delta=10
+        )
+
+        # Check if input in list view is centered
+        inputbox.send_keys('testing')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: testing')
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.locatio['x'] + inputbox.size['width'] / 2,
+            512,
+            delta=10
+        )
+
     def wait_for_row_in_list_table(self, row_text):
         start_time = time.time()
         while True:
