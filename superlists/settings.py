72bd884cd7ff01d44a3b95ed657ea57224eda8ff
eryk <eryk.malczyk@bitcomp.fi>
2021-04-04 18:15:00 +0200

    Add usage of env vars for DEBUG mode, ALLOWED_HOSTS and SECRET_KEY settings

--- superlists/settings.py
+++ superlists/settings.py
@@ -19,11 +19,6 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'x(m+j2#9k_v34nlvj=zfd8is4xip(g9*w*5z(w2mh48oh+aouf'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
 ALLOWED_HOSTS = ['134.122.91.86', 'lists.erkm.pl', 'staging-lists.erkm.pl', 'localhost']
 
@@ -123,3 +118,19 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # STATICFILES_DIRS = (
 #     os.path.join(BASE_DIR, "static"),
 # )
+global_shit = 0
+if 'DJANG_DEBUG_FALSE' in os.environ:
+    DEBUG = False
+    SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
+    ALLOWED_HOSTS = [os.environ['SITENAME']]
+else:
+    print('You start application in insecure mode.\n Are you sure about that */n(n for no)')
+    decision = input('Decision - ')
+    if decision.lower() == 'n':
+        global_shit += 1
+        print(global_shit)
+        import sys
+        sys.exit()
+    DEBUG = True
+    SECRET_KEY = 'x(m+j2#9k_v34nlvj=zfd8is4xip(g9*w*5z(w2mh48oh+aouf'
+    ALLOWED_HOSTS = []
